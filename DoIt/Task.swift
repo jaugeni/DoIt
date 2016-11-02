39e99673477a89a9d96c46a740989f16fcae84fd
jaugeni <jaugeni@gmail.com>
2016-11-02 13:58:44 -0400

    View controller done

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by YAUHENI IVANIUK on 11/2/16.
+//  Copyright © 2016 be connected club inc. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
