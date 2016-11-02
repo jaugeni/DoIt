b521d1b6e9bfbfd453f2a06804af6ed7ecb4e915
jaugeni <jaugeni@gmail.com>
2016-11-02 15:24:34 -0400

    Creating Tasks

---
+++ DoIt/CreateTask.swift
@@ -0,0 +1,30 @@
+//
+//  CreateTask.swift
+//  DoIt
+//
+//  Created by YAUHENI IVANIUK on 11/2/16.
+//  Copyright © 2016 be connected club inc. All rights reserved.
+//
+
+import UIKit
+
+class CreateTask: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importentSwitch: UISwitch!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    
+    }
+    
+    
+}
